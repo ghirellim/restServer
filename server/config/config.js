ce9f2decccace1e2ee47210356ad1251afa9ab9c
Mariano Ghirelli <ghirellisoft@gmail.com>
2019-07-18 02:11:12 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:soylomas@cluster0-gyrgo.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
